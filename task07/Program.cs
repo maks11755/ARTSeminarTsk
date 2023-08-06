f7d13e29755cd62f1e8c9780bc2e47dde4a42f6d
maksim <your>
2023-08-07 10:07:49 +1100

    added decision task07

--- task07/Program.cs
+++ task07/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает
+// на вход трехзначное число и на выходе показывает
+// последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8 
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.Toint32(Console.ReadLine());
+
+if (number < 0) number*=-1;
+
+if (number >= 100 && number <= 999);
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"Последняя цифра {lastDigit}");
+}
+
+else Console.WriteLine("Введено некорректное число");
